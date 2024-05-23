ae76927d453a7f5a55c4a832c80a0d26e9edc872
asia <851540201@qq.com>
2024-05-23 16:59:41 +0800

    feat(lottery): activity: add model

---
+++ lottery-domain/src/main/java/com/asia/lottery/domain/activity/model/req/ActivityConfigReq.java
@@ -0,0 +1,41 @@
+package com.asia.lottery.domain.activity.model.req;
+
+import com.asia.lottery.domain.activity.model.aggregates.ActivityConfigRich;
+
+public class ActivityConfigReq {
+
+    /**
+     * 活动 ID
+     */
+    private Long activityId;
+
+    /**
+     * 活动配置信息
+     */
+    private ActivityConfigRich activityConfigRich;
+
+    public ActivityConfigReq() {
+
+    }
+
+    public ActivityConfigReq(Long activityId, ActivityConfigRich activityConfigRich) {
+        this.activityId = activityId;
+        this.activityConfigRich = activityConfigRich;
+    }
+
+    public Long getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(Long activityId) {
+        this.activityId = activityId;
+    }
+
+    public ActivityConfigRich getActivityConfigRich() {
+        return activityConfigRich;
+    }
+
+    public void setActivityConfigRich(ActivityConfigRich activityConfigRich) {
+        this.activityConfigRich = activityConfigRich;
+    }
+}
